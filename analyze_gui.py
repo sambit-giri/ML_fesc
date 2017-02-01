9d10013a4b6b1ba5126179f6305dc0dcc7ddfe7d
sambit-giri <sambitgiri@outlook.com>
2017-02-01 08:22:38 +0100

    modified

--- analyze_gui.py
+++ analyze_gui.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 master = Tk()
 
-data_dir = '/disk/dawn-1/sgiri/ML_project/ML_fesc/CLASH_data'
+data_dir = 'CLASH_data'
 lens_fol = glob(data_dir+'/ABELL*')+glob(data_dir+'/MACS*')+glob(data_dir+'/RXJ*')+glob(data_dir+'/MS*')+glob(data_dir+'/CL*')
 mags_fol = glob(data_dir+'/high_magnification/*')
 
